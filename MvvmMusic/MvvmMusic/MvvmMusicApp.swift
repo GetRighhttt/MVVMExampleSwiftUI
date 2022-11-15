d36af216d11c18e18c3d47420416264a85985142
Stefan Bayne <stefanbayne@gmail.com>
2022-11-15 10:42:28 -0500

    First commit.

---
+++ MvvmMusic/MvvmMusic/MvvmMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MvvmMusicApp.swift
+//  MvvmMusic
+//
+//  Created by Stefan Bayne on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct MvvmMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
